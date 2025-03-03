aee01d12b9c5591ce2f4069ff7ce484ab48b60b4
PatsFan12 <tamas.deme.andras12@gmail.com>
2025-03-03 18:52:32 +0100

    refactor

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import NavigationBar from './Navbar/navbar.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <NavigationBar />
       <App />
     </BrowserRouter>
   </StrictMode>,
